cca9aa1e06655b9316cded9c7810739a8cfe4b37
nhuse <nathan.huse@yahoo.com>
2021-07-21 17:11:35 -0500

    commit tree a01ae1a465b43d6d3e1912a9dcaacbee4e96e277

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,4 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe() {
+    return <button onFocus={() => console.log("Good!")} onBlur={() => console.log("Hey! Eyes on me!")}>Eyes on me</button>
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,8 @@
 // Code Keypad Component Here
+
+export default function Keypad() {
+    function change() {
+        console.log("Entering password...");
+    }
+    return <input type="password" onChange={change} placeholder="Enter Password"/>
+}
